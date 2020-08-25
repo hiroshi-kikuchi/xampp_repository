7178719dadf4ad12cf1fcd058fe8f38a494ca901
hiroshi-kikuchi <69780130+hiroshi-kikuchi@users.noreply.github.com>
2020-08-25 12:43:26 +0900

    Update test.php

--- xampp/htdocs/test.php
+++ xampp/htdocs/test.php
@@ -4,6 +4,7 @@ header('Content-Type: text/html; charset=UTF-8');
 $master_link = null;
 $slave_link  = null;
 
+// MySQLとのコネクションの接続
 try
 {
 	$master_link	= mysqli_connect( 'localhost', 'hiroshi', 'hiroshi', 'auth', 3306 );
@@ -12,26 +13,27 @@ try
 	$master_link->set_charset( 'UTF-8' );
 	$slave_link->set_charset( 'UTF-8' );
 
-	if ($master_link->connect_error)
+	if( $master_link->connect_error )
 	{
 	    $sql_error = $master_link->connect_error;
 	    error_log($sql_error);
 	    die($sql_error);
 	}
 
-	if ($slave_link->connect_error)
+	if( $slave_link->connect_error )
 	{
 	    $sql_error = $slave_link->connect_error;
 	    error_log($sql_error);
 	    die($sql_error);
 	}
 }
-catch(Excption $ex)
+catch( Excption $ex )
 {
 	error_log( $ex->getMessage() );
 	return FALSE;
 }
 
+// データの追加
 try
 {
 	$insert_result = $master_link->query('INSERT INTO user_info(name,coin,update_time) values( "saitou", 200, now() )');
@@ -49,39 +51,48 @@ catch( Exception $ex )
 	return FALSE;
 }
 
-$select_result = $slave_link->query( 'select id, name, coin from user_info;' );
-if (!$select_result)
+// データの取得
+try
 {
-    $sql_error = $slave_link->error;
-	$sql_error = $maste_link->error;
-    error_log( $sql_error );
-    die( $sql_error );
+	$select_result = $slave_link->query( 'select id, name, coin from user_info;' );
+	if (!$select_result)
+	{
+	    $sql_error = $slave_link->error;
+		$sql_error = $maste_link->error;
+	    error_log( $sql_error );
+	    die( $sql_error );
+	}
+	else
+	{
+		$line="";
+
+	    while($obj = $select_result->fetch_object())
+	    { 
+			$line.=$obj->id; 
+			$line.=" ";
+			$line.=$obj->name; 
+			$line.=" ";
+			$line.=$obj->coin; 
+			$line.="<br />";
+
+			$update_result = $master_link->query( 'UPDATE user_info SET coin = coin * 1.1 WHERE id = ' . $obj->id );
+			if ( !$update_result )
+			{
+				$sql_error = $maste_link->error;
+				error_log( $sql_error );
+				die( $sql_error );
+			}
+	    } 
+
+		printf( "%s<br />", $line );
+
+	    unset($obj);
+	}
 }
-else
+catch( Exception $ex )
 {
-	$line="";
-
-    while($obj = $select_result->fetch_object())
-    { 
-		$line.=$obj->id; 
-		$line.=" ";
-		$line.=$obj->name; 
-		$line.=" ";
-		$line.=$obj->coin; 
-		$line.="<br />";
-
-		$update_result = $master_link->query( 'UPDATE user_info SET coin = coin * 1.1 WHERE id = ' . $obj->id );
-		if ( !$update_result )
-		{
-			$sql_error = $maste_link->error;
-			error_log( $sql_error );
-			die( $sql_error );
-		}
-    } 
-
-	printf( "%s<br />", $line );
-
-    unset($obj);
+	error_log( $ex->getMessage() );
+	return FALSE;
 }
 
 mysqli_close( $master_link );
